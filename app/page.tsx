bcb516f60d9411df31c0c6a7bf16fd865c63f064
InfiniteTrident23 <siddharthnigam002@gmail.com>
2025-05-31 16:26:44 +0530

    checking branch deployment

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
                 variant="outline"
                 className="border-red-500 text-red-500 hover:bg-red-950 hover:text-white hover:scale-105 transition-all"
               >
-                Resume
+                Resumeee
               </Button>
             </Link>
           </motion.div>
